1e320b6ec83819d1cef16cdd55950d035ce09062
Segfault <5221072+Segfaultd@users.noreply.github.com>
2021-10-03 18:43:12 +0200

    Use framework launcher

--- code/launcher/src/main.cpp
+++ code/launcher/src/main.cpp
@@ -1,3 +1,18 @@
-int main(int argc, char **argv){
-    return 1;
+#include <Windows.h>
+
+#include <launcher/project.h>
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd) {
+    Framework::Launcher::ProjectConfiguration config;
+    config.destinationDllName = L"client.dll";
+    config.executableName     = L"mafiadefinitiveedition.exe";
+    config.name               = "MafiaMP";
+    config.platform           = Framework::Launcher::ProjectPlatform::STEAM;
+    config.steamAppId         = 1030840;
+
+    Framework::Launcher::Project project(config);
+    if (!project.Launch()) {
+        return 1;
+    }
+    return 0;
 }
