8d10164e1cd5cc587f618527e987652f9e14ad22
Dominik Madarász <zaklaus@madaraszd.net>
2021-10-05 17:21:17 +0200

    Improve linking flags

--- code/launcher/src/main.cpp
+++ code/launcher/src/main.cpp
@@ -2,8 +2,19 @@
 
 #include <launcher/project.h>
 
-#pragma bss_seg(".maf")
-char maf_seg[0x10000000];
+#pragma comment(linker, "/merge:.data=.cld")
+#pragma comment(linker, "/merge:.rdata=.clr")
+#pragma comment(linker, "/merge:.cl=.zdata")
+#pragma comment(linker, "/merge:.text=.zdata")
+#pragma comment(linker, "/section:.zdata,re")
+
+#pragma bss_seg(".mmp")
+char guest_seg[0x14000000];
+
+char stub_seg[0x100000];
+
+#pragma data_seg(".zdata")
+char zdata[200] = {1};
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd) {
     Framework::Launcher::ProjectConfiguration config;
