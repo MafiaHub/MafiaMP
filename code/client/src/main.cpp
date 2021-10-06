ea1e2f97e07db16f19530ebde0d6a67ebadd4958
Segfault <5221072+Segfaultd@users.noreply.github.com>
2021-10-06 14:17:54 +0200

    don't load hints atm

--- code/client/src/main.cpp
+++ code/client/src/main.cpp
@@ -19,7 +19,7 @@ extern "C" void __declspec(dllexport) InitClient() {
 
     MH_Initialize();
 	hook::set_base();
-    hook::load_hints();
+    // hook::load_hints();
 
     // Load the patterns
     SDK::Patterns::InitPatterns();
