cc570b7bd408936d76b3b264b279d91133bbe389
Segfault <5221072+Segfaultd@users.noreply.github.com>
2021-10-10 18:19:05 +0200

    Basic server instance setup

--- code/server/src/main.cpp
+++ code/server/src/main.cpp
@@ -1,3 +1,22 @@
-int main(int argc, char **arv){
-    return 1;
+#include "core/server.h"
+
+int main(int argc, char **arv) {
+    Framework::Integrations::Server::InstanceOptions opts;
+    opts.bindHost      = "0.0.0.0";
+    opts.bindPort      = 27015;
+    opts.bindName      = "Mafia MP Local Dev Server";
+    opts.maxPlayers    = 10;
+    opts.modName       = "MafiaMP";
+    opts.modSlug       = "mafia_mp";
+    opts.modVersion    = "0.1.0";
+    opts.bindPassword  = "";
+    opts.enableSignals = true;
+
+    MafiaMP::Server server;
+    if (server.Init(opts) != Framework::Integrations::Server::ServerError::SERVER_NONE) {
+        return 1;
+    }
+    server.Run();
+    server.Shutdown();
+    return 0;
 }
