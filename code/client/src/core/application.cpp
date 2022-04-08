c8de010ad05561deac7fde39474d88f4db859a52
Dominik Madarász <zaklaus@outlook.com>
2022-04-08 10:16:45 +0200

    vendors: update flecs

--- code/client/src/core/application.cpp
+++ code/client/src/core/application.cpp
@@ -193,7 +193,7 @@ namespace MafiaMP::Core {
         const auto net = GetNetworkingEngine()->GetNetworkClient();
 
         net->RegisterMessage<Shared::Messages::Human::HumanSpawn>(Shared::Messages::ModMessages::MOD_HUMAN_SPAWN, [this](SLNet::RakNetGUID guid, Shared::Messages::Human::HumanSpawn *msg) {
-            const auto e = GetWorldEngine()->GetEntityByServerID(msg->GetServerID());
+            auto e = GetWorldEngine()->GetEntityByServerID(msg->GetServerID());
             if (!e.is_alive()) {
                 return;
             }
