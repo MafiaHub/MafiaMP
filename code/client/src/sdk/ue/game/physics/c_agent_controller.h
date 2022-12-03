89cf528afc2334bd180447db055ded2f4f3a77c6
Segfault <5221072+Segfaultd@users.noreply.github.com>
2022-12-03 18:40:33 +0100

    Added rigid body class

--- code/client/src/sdk/ue/game/physics/c_agent_controller.h
+++ code/client/src/sdk/ue/game/physics/c_agent_controller.h
@@ -1,7 +1,13 @@
 #pragma once
 
+#include "c_rigid_body.h"
+
 namespace SDK {
     namespace ue::game::physics {
-        class C_AgentController {};
+        class C_AgentController {
+          public:
+            char pad0[0x80];                // 0000 - 0080
+            C_RigidBody *m_pRigidBody;      // 0080 - 0088
+        };
     }
 }
