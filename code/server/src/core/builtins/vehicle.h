bc5e497f70a16d8f658da861bb2609652d1b6339
Dominik Madarász <zaklaus@outlook.com>
2022-12-06 13:16:10 +0100

    drop Set/GetHorn

--- code/server/src/core/builtins/vehicle.h
+++ code/server/src/core/builtins/vehicle.h
@@ -17,18 +17,6 @@ namespace MafiaMP::Scripting {
             return ss.str();
         }
 
-        void SetHorn(bool state) {
-            auto data = _ent.get_mut<Shared::Modules::VehicleSync::UpdateData>();
-            data->horn = state;
-
-            FW_SEND_COMPONENT_GAME_RPC(Shared::RPC::SetVehicleData, _ent, *data);
-        }
-
-        bool GetHorn() {
-            const auto data = _ent.get<Shared::Modules::VehicleSync::UpdateData>();
-            return data->horn;
-        }
-
         static void Register(v8::Isolate *isolate, v8pp::module *rootModule) {
             if (!rootModule) {
                 return;
@@ -36,8 +24,6 @@ namespace MafiaMP::Scripting {
 
             v8pp::class_<Vehicle> cls(isolate);
             cls.inherit<Framework::Integrations::Scripting::Entity>();
-            cls.function("setHorn", &Vehicle::SetHorn);
-            cls.function("getHorn", &Vehicle::GetHorn);
             rootModule->class_("Vehicle", cls);
         }
     };
