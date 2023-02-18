12c433144279aa6f7adc41867eaa2cae15900d86
Dominik Madarász <zaklaus@outlook.com>
2023-02-18 22:38:24 +0100

    Chat and command handlers

--- code/server/src/core/builtins/world.h
+++ code/server/src/core/builtins/world.h
@@ -8,7 +8,10 @@
 
 #include "../../shared/rpc/environment.h"
 
+#include "shared/rpc/chat_message.h"
+
 #include "vehicle.h"
+#include "player.h"
 
 #include "core_modules.h"
 
@@ -44,6 +47,34 @@ namespace MafiaMP::Scripting {
             FW_SEND_COMPONENT_RPC(MafiaMP::Shared::RPC::SetEnvironment, {}, weather->_dayTimeHours);
         }
 
+        static void SendChatMessage(std::string message, Human *human) {
+            if (human) {
+                const auto ent = human->GetHandle();
+                const auto str   = ent.get<Framework::World::Modules::Base::Streamer>();
+                if (!str)
+                    return;
+                FW_SEND_COMPONENT_RPC_TO(Shared::RPC::ChatMessage, SLNet::RakNetGUID(str->guid), message);
+            }
+        }
+
+        static void BroadcastMessage(std::string message) {
+            FW_SEND_COMPONENT_RPC(Shared::RPC::ChatMessage, message);
+        }
+
+        static void OnChatMessage(flecs::entity e, std::string message) {
+            Framework::CoreModules::GetScriptingModule()->ForEachResource([&](Framework::Scripting::Engines::IResource *resource) {
+                auto nodeResource = reinterpret_cast<Framework::Scripting::Engines::Node::Resource *>(resource);
+                nodeResource->InvokeEvent("chatMessage", Human::WrapHuman(nodeResource, e), message);
+            });
+        }
+
+        static void OnChatCommand(flecs::entity e, std::string message, std::string command, std::vector<std::string> args) {
+            Framework::CoreModules::GetScriptingModule()->ForEachResource([&](Framework::Scripting::Engines::IResource *resource) {
+                auto nodeResource = reinterpret_cast<Framework::Scripting::Engines::Node::Resource *>(resource);
+                nodeResource->InvokeEvent("chatCommand", Human::WrapHuman(nodeResource, e), message, command, args);
+            });
+        }
+
         static void Register(v8::Isolate *isolate, v8pp::module *rootModule) {
             // Create the environment namespace
             v8pp::module environment(isolate);
@@ -55,6 +86,9 @@ namespace MafiaMP::Scripting {
             v8pp::module world(isolate);
             world.function("createVehicle", &World::CreateVehicle);
             rootModule->submodule("World", world);
+
+            rootModule->function("sendChatMessage", &World::SendChatMessage);
+            rootModule->function("broadcastMessage", &World::BroadcastMessage);
         }
     };
 } // namespace MafiaMP::Scripting
--- code/server/src/core/server.cpp
+++ code/server/src/core/server.cpp
@@ -59,7 +59,7 @@ namespace MafiaMP {
             // Broadcast chat message
             const auto st  = player.get<Framework::World::Modules::Base::Streamer>();
             const auto msg = fmt::format("Player {} has joined the session!", st->nickname);
-            BroadcastChatMessage(player, msg);
+            BroadcastChatMessage(msg);
 
             Scripting::Human::EventPlayerConnected(player);
         });
@@ -67,7 +67,7 @@ namespace MafiaMP {
         SetOnPlayerDisconnectCallback([this](flecs::entity player, uint64_t) {
             const auto st  = player.get<Framework::World::Modules::Base::Streamer>();
             const auto msg = fmt::format("Player {} has left the session!", st->nickname);
-            BroadcastChatMessage(player, msg);
+            BroadcastChatMessage(msg);
 
             Scripting::Human::EventPlayerDisconnected(player);
         });
@@ -88,9 +88,8 @@ namespace MafiaMP {
         MafiaMP::Scripting::Builtins::Register(nodeSDK->GetIsolate(), nodeSDK->GetModule());
     }
 
-    void Server::BroadcastChatMessage(flecs::entity ent, const std::string &msg) {
+    void Server::BroadcastChatMessage(const std::string &msg) {
         FW_SEND_COMPONENT_RPC(Shared::RPC::ChatMessage, msg);
-        Framework::Logging::GetLogger("chat")->info(fmt::format("[{}] {}", ent.id(), msg));
     }
     void Server::InitRPCs() {
         const auto net = GetNetworkingEngine()->GetNetworkServer();
@@ -102,9 +101,24 @@ namespace MafiaMP {
             if (!ent.is_alive())
                 return;
 
-            const auto st  = ent.get<Framework::World::Modules::Base::Streamer>();
-            const auto msg = fmt::format("{}: {}", st->nickname, chatMessage->GetText());
-            BroadcastChatMessage(ent, msg);
+            const auto text = chatMessage->GetText();
+            if (text[0] == '/') {
+                const auto command  = text.substr(1, text.find(' ') - 1);
+                const auto argsPart = text.substr(text.find(' ') + 1);
+                std::vector<std::string> args;
+                std::string arg;
+                std::istringstream iss(argsPart);
+                while (iss >> arg) {
+                    args.push_back(arg);
+                }
+                Scripting::World::OnChatCommand(ent, text, command, args);
+            } else {
+                Scripting::World::OnChatMessage(ent, text);
+            }
+
+            // const auto st  = ent.get<Framework::World::Modules::Base::Streamer>();
+            // const auto msg = fmt::format("{}: {}", st->nickname, chatMessage->GetText());
+            // BroadcastChatMessage(ent, msg);
         });
 
         // spawn car rpc TEST
