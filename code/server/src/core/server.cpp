668f9779dc30dd25913a1d151be6bc1f1ba7e2bd
Dominik Madarász <zaklaus@outlook.com>
2023-02-25 20:13:07 +0100

    remove remote car spawn

--- code/server/src/core/server.cpp
+++ code/server/src/core/server.cpp
@@ -15,12 +15,8 @@
 
 #include <fmt/format.h>
 
-// VERY UGLY
-extern std::vector<std::string> TEST_vehiclelist;
-
 #include "shared/game_rpc/human/human_changeskin.h"
-#include "shared/rpc/spawn_car.h"
-namespace MafiaMP {
+ namespace MafiaMP {
 
     void Server::PostInit() {
         _serverRef = this;
@@ -121,31 +117,6 @@ namespace MafiaMP {
             // BroadcastChatMessage(ent, msg);
         });
 
-        // spawn car rpc TEST
-        net->RegisterRPC<Shared::RPC::SpawnCar>([this](SLNet::RakNetGUID guid, Shared::RPC::SpawnCar *msg) {
-            if (!msg->Valid()) {
-                return;
-            }
-
-            const auto ent = GetWorldEngine()->GetEntityByGUID(guid.g);
-            if (!ent.is_alive())
-                return;
-
-            if (std::find(TEST_vehiclelist.begin(), TEST_vehiclelist.end(), msg->GetModelName()) == TEST_vehiclelist.end()) {
-                return;
-            }
-
-            auto t      = ent.get<Framework::World::Modules::Base::Transform>();
-            auto carEnt = MafiaMP::Core::Modules::Vehicle::Create(this);
-
-            auto carT = carEnt.get_mut<Framework::World::Modules::Base::Transform>();
-            carT->pos = t->pos;
-            carT->rot = t->rot;
-
-            auto frame       = carEnt.get_mut<Framework::World::Modules::Base::Frame>();
-            frame->modelName = msg->GetModelName();
-        });
-
         // test until we do it via nodejs builtins
         net->RegisterGameRPC<Shared::RPC::HumanChangeSkin>([this](SLNet::RakNetGUID guid, Shared::RPC::HumanChangeSkin *msg) {
             if (!msg->Valid())
@@ -161,61 +132,3 @@ namespace MafiaMP {
         });
     }
 } // namespace MafiaMP
-
-std::vector<std::string> TEST_vehiclelist = {
-    "berkley_810",
-    "betting_office",
-    "bolt_ace",
-    "bolt_ace_pickup",
-    "bolt_cooler",
-    "bolt_delivery",
-    "bolt_delivery_amb",
-    "bolt_hearse",
-    "bolt_mail",
-    "bolt_model_b",
-    "bolt_pickup",
-    "bolt_truck",
-    "bolt_truck_scripted_farm",
-    "bolt_v8",
-    "brubaker_forte",
-    "bulworth_packhard",
-    "bulworth_sentry",
-    "carrozella_c_series",
-    "celeste_mark_5",
-    "crazy_horse",
-    "culver_airmaster",
-    "disorder",
-    "eckhart_crusader",
-    "eckhart_elite",
-    "eckhart_fletcher",
-    "falconer_classic",
-    "flame_spear",
-    "hank_a",
-    "haverley_tomahawk_p",
-    "houston_coupe",
-    "lassiter_v16",
-    "lassiter_v16_appolyon",
-    "lassiter_v16_roadster",
-    "manta_prototype",
-    "moto_blackcats",
-    "motorcycle_1935",
-    "mutagen",
-    "parry_bus",
-    "samson_drifter",
-    "samson_tanker",
-    "shubert_e_six_det",
-    "shubert_e_six_p",
-    "shubert_e_six",
-    "shubert_e_six_taxi",
-    "shubert_frigate",
-    "shubert_six",
-    "shubert_six_det",
-    "shubert_six_p",
-    "shubert_six_taxi",
-    "smith_moray",
-    "smith_thrower",
-    "smith_v12_chicago",
-    "smith_v12",
-    "trautenberg_sport",
-    "waybar_concept",
-};
