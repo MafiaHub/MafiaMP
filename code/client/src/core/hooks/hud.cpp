4c4c3fa7930abe67d24ab536de8b83c0a915f192
Segfault <5221072+Segfaultd@users.noreply.github.com>
2023-03-04 18:34:02 +0100

    Reversed C_MenuBase::OnScriptMenuEvent

---
+++ code/client/src/core/hooks/hud.cpp
@@ -0,0 +1,22 @@
+#include <utils/safe_win32.h>
+#include <MinHook.h>
+
+#include <utils/hooking/hook_function.h>
+#include <utils/hooking/hooking.h>
+
+#include <logging/logger.h>
+
+#include "../../sdk/mafia/ui/menu/c_menu_base.h"
+
+typedef int64_t(__fastcall *C_MenuBase__OnScriptedMenuEvent_t)(void *pThis, int, SDK::mafia::ui::menu::C_MenuBase::E_ScriptedMenuEvent);
+C_MenuBase__OnScriptedMenuEvent_t C_MenuBase__OnScriptedMenuEvent_original = nullptr;
+int64_t C_MenuBase__OnScriptedMenuEvent(void *pThis, int unk, SDK::mafia::ui::menu::C_MenuBase::E_ScriptedMenuEvent menuEvent) {
+    // TODO: broadcast over network
+    Framework::Logging::GetLogger("Hooks")->debug("C_MenuBase::OnScriptedMenuEvent: unk {} event {}", unk, menuEvent);
+    return C_MenuBase__OnScriptedMenuEvent_original(pThis, unk, menuEvent);
+}
+
+static InitFunction init([]() {
+    const auto C_MenuBase__OnScriptedMenuEvent_Addr = hook::get_opcode_address("E8 ? ? ? ? 83 BE ? ? ? ? ? 75 0E");
+    MH_CreateHook((LPVOID)C_MenuBase__OnScriptedMenuEvent_Addr, (PBYTE)C_MenuBase__OnScriptedMenuEvent, reinterpret_cast<void **>(&C_MenuBase__OnScriptedMenuEvent_original));
+});
--- code/client/src/sdk/mafia/ui/menu/c_menu_base.h
+++ code/client/src/sdk/mafia/ui/menu/c_menu_base.h
@@ -1,9 +1,24 @@
 #pragma once
 
+#include <cstdint>
+
 namespace SDK {
     namespace mafia::ui::menu {
         class C_MenuBase {
           public:
+            enum class E_ScriptedMenuEvent : uint8_t {
+                ACCEPT = 0,
+                BACK = 1,
+                ACCEPT_X = 2,
+                ACCEPT_Y = 3,
+                VIEWED = 4,
+                CLEAR_MESSAGES_QUEUE = 5,
+                ALLOW_X_INPUT = 6,
+                CREDITS_FINISHED = 7,
+                UNK_TICK = 8,
+                UNK_LOADING = 12,
+                UNK_LOADING_2 = 13
+            };
         };
     } // namespace mafia::ui::menu
 } // namespace SDK
