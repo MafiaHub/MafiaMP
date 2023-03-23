2c53a70eae039ea9f853f3e6b41cd7b90e0bed85
Segfault <5221072+Segfaultd@users.noreply.github.com>
2023-03-23 23:49:59 +0100

    Improvements to ultralight renderer

--- code/client/src/core/ui/web.cpp
+++ code/client/src/core/ui/web.cpp
@@ -1,6 +1,9 @@
 #include "web.h"
 
 #include <AppCore/Platform.h>
+#include <logging/logger.h>
+
+#include "../../game/module.h"
 
 namespace MafiaMP::Core::UI {
     bool Web::Init() {
@@ -9,6 +12,20 @@ namespace MafiaMP::Core::UI {
         config.resource_path = "./resources/";
         config.use_gpu_renderer = false;
         config.device_scale = 1.0;
+
+        // Create our GPU driver
+        const auto renderDevice = Game::gGlobals.renderDevice;
+        if (!renderDevice) {
+            Framework::Logging::GetLogger("UI")->error("Failed to grab game render device");
+            return false;
+        }
+        _device = renderDevice->_device;
+        _context = renderDevice->_context;
+
+        if (!_device || !_context) {
+            Framework::Logging::GetLogger("UI")->error("Failed to grab game render device or context ({} {})", fmt::ptr(_device), fmt::ptr(_context));
+            return false;
+        }
 
         // Configure the platform via the singleton
         ultralight::Platform::instance().set_config(config);
@@ -19,6 +36,7 @@ namespace MafiaMP::Core::UI {
         // Create our renderer
         _renderer = ultralight::Renderer::Create();
         if (!_renderer || !_renderer.get()) {
+            Framework::Logging::GetLogger("UI")->error("Failed to create renderer");
             return false;
         }
 
@@ -28,6 +46,7 @@ namespace MafiaMP::Core::UI {
     bool Web::CreateView(std::string name, int width, int height, std::string url) {
         ultralight::RefPtr<ultralight::View> view = _renderer->CreateView(width, height, false, nullptr);
         if (!view || !view.get()) {
+            Framework::Logging::GetLogger("UI")->error("Failed to create view");
             return false;
         }
 
@@ -35,12 +54,14 @@ namespace MafiaMP::Core::UI {
         view->Focus();
 
         _views.emplace(name, view);
+        Framework::Logging::GetLogger("UI")->debug("Created view {}", name.c_str());
         return true;
     }
 
     bool Web::ToggleViewFocus(std::string name) {
         const auto view = _views[name];
         if (!view || !view.get()) {
+            Framework::Logging::GetLogger("UI")->error("Failed to find view");
             return false;
         }
 
@@ -51,6 +72,58 @@ namespace MafiaMP::Core::UI {
             view->Focus();
         }
         return true;
+    }
+
+    void Web::ConvertPixelsToARGB(uint8_t *pixels, uint32_t width, uint32_t height, uint32_t stride) {
+        for (uint32_t y = 0; y < height; y++) {
+            for (uint32_t x = 0; x < width; x++) {
+                uint8_t *pixel = pixels + (y * stride) + (x * 4);
+                std::swap(pixel[0], pixel[2]);
+            }
+        }
+    }
+
+    void Web::CopyPixelsToTexture(void *pixels, uint32_t width, uint32_t height, uint32_t stride) {
+        if (!_texture) {
+            D3D11_TEXTURE2D_DESC textDesc;
+            textDesc.Width     = width;
+            textDesc.Height    = height;
+            textDesc.MipLevels = textDesc.ArraySize = 1;
+            textDesc.Format                         = DXGI_FORMAT_B8G8R8A8_UNORM;
+            textDesc.SampleDesc.Count               = 1;
+            textDesc.SampleDesc.Quality             = 0;
+            textDesc.Usage                          = D3D11_USAGE_DYNAMIC;
+            textDesc.BindFlags                      = D3D11_BIND_SHADER_RESOURCE;
+            textDesc.CPUAccessFlags                 = D3D11_CPU_ACCESS_WRITE;
+            textDesc.MiscFlags                      = 0;
+
+            HRESULT result = _device->CreateTexture2D(&textDesc, nullptr, &_texture);
+            if (FAILED(result)) {
+                Framework::Logging::GetLogger("UI")->error("Failed to create 2D Texture");
+                return;
+            }
+        }
+
+        D3D11_MAPPED_SUBRESOURCE mappedResource;
+        const auto result = _context->Map(_texture, 0, D3D11_MAP_WRITE_DISCARD, 0, &mappedResource);
+
+        if (SUCCEEDED(result)) {
+            const auto pDest = static_cast<uint8_t *>(mappedResource.pData);
+            std::memcpy(pDest, pixels, width * height * 4);
+        }
+        _context->Unmap(_texture, 0);
+    }
+
+    void Web::CopyBitmapToTexture(ultralight::RefPtr<ultralight::Bitmap> bitmap) {
+        void *pixels = bitmap->LockPixels();
+
+        uint32_t width  = bitmap->width();
+        uint32_t height = bitmap->height();
+        uint32_t stride = bitmap->row_bytes();
+
+        CopyPixelsToTexture(pixels, width, height, stride);
+
+        bitmap->UnlockPixels();
     }
 
     void Web::Update() {
@@ -67,5 +140,19 @@ namespace MafiaMP::Core::UI {
         }
 
         _renderer->Render();
+
+        for (auto view : _views) {
+            // Acquire the bitmap surface
+            ultralight::BitmapSurface *surface = (ultralight::BitmapSurface *)(view.second->surface());
+
+            // Check if the surface is dirty (pixels changed)
+            if (!surface->dirty_bounds().IsEmpty()) {
+                // Copy the bitmap to our GPU texture
+                CopyBitmapToTexture(surface->bitmap());
+
+                // Mark the surface as clean
+                surface->ClearDirtyBounds();
+            }
+        }
     }
 }
--- code/client/src/core/ui/web.h
+++ code/client/src/core/ui/web.h
@@ -1,22 +1,35 @@
 #pragma once
-
+#include <utils/safe_win32.h>
 #include <ultralight/Ultralight.h>
 
 #include <string>
 #include <unordered_map>
 
+#include <d3d11.h>
+
 namespace MafiaMP::Core::UI {
     class Web {
+      private:
+        ID3D11Device *_device;
+        ID3D11DeviceContext *_context;
+        ID3D11Texture2D *_texture;
+        ID3D11ShaderResourceView *_textureView;
+
       protected:
         ultralight::RefPtr<ultralight::Renderer> _renderer;
         std::unordered_map<std::string, ultralight::RefPtr<ultralight::View>> _views;
 
-        public:
-          bool Init();
-          bool CreateView(std::string name, int width, int height, std::string url);
-          bool ToggleViewFocus(std::string name);
+      public:
+        bool Init();
+        bool CreateView(std::string name, int width, int height, std::string url);
+        bool ToggleViewFocus(std::string name);
 
-          void Update();
-          void Render();
+        void Update();
+        void Render();
+
+      private:
+        void CopyBitmapToTexture(ultralight::RefPtr<ultralight::Bitmap>);
+        void CopyPixelsToTexture(void *pixels, uint32_t width, uint32_t height, uint32_t stride);
+        void ConvertPixelsToARGB(uint8_t *pixels, uint32_t width, uint32_t height, uint32_t stride);
     };
 }
