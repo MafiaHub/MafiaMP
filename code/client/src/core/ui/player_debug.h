161a16a0ff73849cc16a8f9c324a2dc6aafccb3c
Deewarz <deewarz@gmail.com>
2024-01-15 17:31:49 +0100

    Cleanup imports

--- code/client/src/core/ui/player_debug.h
+++ code/client/src/core/ui/player_debug.h
@@ -2,14 +2,6 @@
 
 #include "utils/safe_win32.h"
 
-#include <fu2/function2.hpp>
-
-#include <string>
-#include <unordered_map>
-#include <vector>
-
-#include "sdk/entities/c_entity.h"
-
 namespace MafiaMP::Core::UI {
     class PlayerDebug final {
       public:
