2c237d9d9b83863769a1e783eab3af4bf865a03b
Segfault <5221072+Segfaultd@users.noreply.github.com>
2024-01-21 08:26:51 +0100

    base c_variant

--- code/client/src/sdk/ue/c_variant.h
+++ code/client/src/sdk/ue/c_variant.h
@@ -1,7 +1,22 @@
 #pragma once
 
+#include "c_string.h"
+
 namespace SDK {
     namespace ue {
-        class C_Variant;
+        namespace sys::core {
+            enum E_ColorSetType {};
+        };
+
+        class C_Variant {
+          public:
+            C_Variant(C_String const &) = delete;
+            ~C_Variant()                      = delete;
+
+            C_Variant *swap(C_Variant &other) = delete;
+
+            C_Variant *operator=(C_Variant const &other) = delete;
+            C_Variant *operator=(C_Variant const &&other) = delete;
+        };
     }
 }
