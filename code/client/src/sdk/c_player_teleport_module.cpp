0ca0e6b65cfb4479e7e766781fa6d014924616ca
Deewarz <deewarz@gmail.com>
2024-01-24 18:13:41 +0100

    Fixes for C_PlayerTeleportModule
    - extends C_TicketModule
    - C_PlayerTeleportModule::TeleportPlayer returns this
    - Move GetInstance to the bottom

--- code/client/src/sdk/c_player_teleport_module.cpp
+++ code/client/src/sdk/c_player_teleport_module.cpp
@@ -1,12 +1,6 @@
 #include "c_player_teleport_module.h"
 
-#include "patterns.h"
-
 namespace SDK {
-    C_PlayerTeleportModule *C_PlayerTeleportModule::GetInstance() {
-        return *reinterpret_cast<C_PlayerTeleportModule **>(gPatterns.C_PlayerTeleportModule__Instance);
-    }
-
     /**
      * Controls are automatically locked during the transition (loading the world).
      * If you are in a car, your car will also be teleported with you in it
@@ -16,11 +10,15 @@ namespace SDK {
      * @param noFadeEffects - disable fade effects (no fade in, no fade out)
      * @param unk4
      */
-    void C_PlayerTeleportModule::TeleportPlayer(ue::C_CntPtr<uintptr_t> &syncObject, ue::sys::math::C_Vector const &pos, ue::sys::math::C_Vector const &dir, bool unk1, bool noFadeIn, bool noFadeEffects, bool unk4) {
-        hook::this_call(gPatterns.C_PlayerTeleportModule__TeleportPlayer, this, syncObject, pos, dir, unk1, noFadeIn, noFadeEffects, unk4);
+    C_PlayerTeleportModule *C_PlayerTeleportModule::TeleportPlayer(ue::C_CntPtr<uintptr_t> &syncObject, ue::sys::math::C_Vector const &pos, ue::sys::math::C_Vector const &dir, bool unk1, bool noFadeIn, bool noFadeEffects, bool unk4) {
+        return hook::this_call<C_PlayerTeleportModule *>(gPatterns.C_PlayerTeleportModule__TeleportPlayer, this, syncObject, pos, dir, unk1, noFadeIn, noFadeEffects, unk4);
     }
 
     C_PlayerTeleportModule *GetPlayerTeleportModule() {
         return hook::call<C_PlayerTeleportModule *>(gPatterns.C_PlayerTeleportModule__GetPlayerTeleportModule);
+    }
+
+    C_PlayerTeleportModule *C_PlayerTeleportModule::GetInstance() {
+        return *reinterpret_cast<C_PlayerTeleportModule **>(gPatterns.C_PlayerTeleportModule__Instance);
     }
 } // namespace SDK
--- code/client/src/sdk/c_player_teleport_module.h
+++ code/client/src/sdk/c_player_teleport_module.h
@@ -1,17 +1,19 @@
 #pragma once
 
-#include <cstdint>
+#include "patterns.h"
 
-#include "ue/sys/math/c_vector.h"
+#include "c_ticked_module.h"
+
 #include "ue/c_cnt_ptr.h"
+#include "ue/sys/math/c_vector.h"
 
 namespace SDK {
-    class C_PlayerTeleportModule {
+    class C_PlayerTeleportModule: public C_TickedModule {
       public:
-        static C_PlayerTeleportModule *GetInstance();
+        C_PlayerTeleportModule *TeleportPlayer(ue::C_CntPtr<uintptr_t> &, ue::sys::math::C_Vector const &, ue::sys::math::C_Vector const &, bool, bool, bool, bool);
 
-        void TeleportPlayer(ue::C_CntPtr<uintptr_t> &, ue::sys::math::C_Vector const &, ue::sys::math::C_Vector const &, bool, bool, bool, bool);
+        static C_PlayerTeleportModule *GetInstance();
     };
 
     C_PlayerTeleportModule *GetPlayerTeleportModule();
-}
+} // namespace SDK
