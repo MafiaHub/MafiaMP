04ade3e4af54f1a8b2cea5abe7c96898a07a347c
Deewarz <72361937+Deewarz@users.noreply.github.com>
2024-01-28 01:36:30 +0100

    Fix Revert "Vehicle DB improvements (#44)" (#70)
    * Fix Revert "Vehicle DB improvements (#44)"
    
    * Use relative import in SDK
    
    * Keep C_WeakPtr properties public
    
    * Accessors are back

--- code/client/src/sdk/ue/c_weak_ptr.h
+++ code/client/src/sdk/ue/c_weak_ptr.h
@@ -8,10 +8,22 @@ namespace SDK {
         class C_WeakPtr {
           public:
             uintptr_t unk;
-            T* m_Ptr = nullptr;
+            T *m_Ptr = nullptr;
             uintptr_t unk3;
             uintptr_t unk4;
             uint8_t paaddd[32];
+
+            T *Get() const {
+                return m_Ptr;
+            }
+
+            bool IsValid() const {
+                return m_Ptr != nullptr;
+            }
+
+            T *operator->() const {
+                return m_Ptr;
+            }
         };
-    }
+    } // namespace ue
 } // namespace SDK
