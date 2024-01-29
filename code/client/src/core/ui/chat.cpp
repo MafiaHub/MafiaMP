bc2fe95a2f3a154238ac6aa7609eda0f761bcc67
Dominik Madarász <zaklaus@outlook.com>
2024-01-29 11:48:30 +0100

    chat history

--- code/client/src/core/ui/chat.cpp
+++ code/client/src/core/ui/chat.cpp
@@ -3,6 +3,8 @@
 #include "core/application.h"
 #include "game/helpers/controls.h"
 
+#include <external/imgui/helpers.hpp>
+
 #include <imgui/imgui.h>
 
 namespace MafiaMP::Core::UI {
@@ -36,10 +38,48 @@ namespace MafiaMP::Core::UI {
         if (_isFocused) {
             ImGui::SetNextItemWidth(ImGui::GetWindowWidth() * 0.95f);
             ImGui::SetKeyboardFocusHere(0);
-            if (ImGui::InputText("##chatinput", _inputText, sizeof(_inputText), ImGuiInputTextFlags_EnterReturnsTrue)) {
+
+            auto inputEditCallback = [&](ImGuiInputTextCallbackData *data) {
+                if (data->EventFlag == ImGuiInputTextFlags_CallbackHistory && data->EventKey == ImGuiKey_UpArrow) {
+                    if (_history.empty())
+                        return 0;
+                    if (_historyPos == -1) {
+                        _tempInputText = data->Buf;
+                    }
+                    else if (_historyPos + 1 == _history.size()) {
+                        return 0;
+                    }
+                    ++_historyPos;
+                    data->DeleteChars(0, data->BufTextLen);
+                    data->InsertChars(0, _history.at(_historyPos).c_str());
+
+                    ImGui::SetKeyboardFocusHere(-1);
+                    _isFocused = true;
+                }
+                else if (data->EventFlag == ImGuiInputTextFlags_CallbackHistory && data->EventKey == ImGuiKey_DownArrow) {
+                    if (_historyPos > 0) {
+                        --_historyPos;
+                        data->DeleteChars(0, data->BufTextLen);
+                        data->InsertChars(0, _history.at(_historyPos).c_str());
+                    }
+                    else if (!_tempInputText.empty()) {
+                        _historyPos = -1;
+                        data->DeleteChars(0, data->BufTextLen);
+                        data->InsertChars(0, _tempInputText.c_str());
+                        _tempInputText = "";
+                    }
+
+                    ImGui::SetKeyboardFocusHere(-1);
+                    _isFocused = true;
+                }
+                return 0;
+            };
+
+            if (ImGui::InputText("##chatinput", _inputText, sizeof(_inputText), ImGuiInputTextFlags_EnterReturnsTrue | ImGuiInputTextFlags_CallbackHistory, Framework::External::ImGUI::getCallback(inputEditCallback), &inputEditCallback)) {
                 _isFocused = false;
                 if (strlen(_inputText)) {
                     onMessageSentProc(_inputText);
+                    _history.emplace(_history.begin(), _inputText);
                     strcpy(_inputText, "");
                 }
 
