a62f1bdb3846ee9fc09c95abfbd2db7ed7210e24
Deewarz <deewarz@gmail.com>
2024-01-31 23:02:12 +0100

    fix pad name

--- code/client/src/sdk/ue/game/traffic/c_streaming_traffic_module.h
+++ code/client/src/sdk/ue/game/traffic/c_streaming_traffic_module.h
@@ -49,7 +49,7 @@ namespace SDK {
             }
 
           private:
-            char pad_0094[0x8C];     // 0008 - 0094
+            char pad_0008[0x8C];     // 0008 - 0094
             int m_SeasonID;          // 0094 - 0098
             int m_PrevSeasonID;      // 0098 - 009C
             char pad_009C[0x19C];    // 009C - 100
