4de573ce7f7dfcdd418d17578167c90f16d8d0a9
Deewarz <deewarz@gmail.com>
2024-02-01 20:30:31 +0100

    Use white as default colors (like the game do)

--- code/shared/modules/vehicle_sync.hpp
+++ code/shared/modules/vehicle_sync.hpp
@@ -18,8 +18,8 @@ namespace MafiaMP::Shared::Modules {
             glm::vec3 angularVelocity {};
             bool beaconLightsOn = false;
             float brake         = 0.0f;
-            glm::vec4 colorPrimary {0.0f, 0.0f, 0.0f, 1.0f};
-            glm::vec4 colorSecondary {0.0f, 0.0f, 0.0f, 1.0f};
+            glm::vec4 colorPrimary {1.0f, 1.0f, 1.0f, 1.0f};
+            glm::vec4 colorSecondary {1.0f, 1.0f, 1.0f, 1.0f};
             float dirt           = 0.0f;
             float fuel           = 100.0f; // We use arbitrary value, the max depends of the vehicle. See C_Motor::GetFuelSettings.
             int gear             = 0;
@@ -30,11 +30,11 @@ namespace MafiaMP::Shared::Modules {
             float power          = 0.0f;
             bool radioOn         = false;
             int radioStationId   = 0;
-            glm::vec4 rimColor {0.0f, 0.0f, 0.0f, 1.0f};
+            glm::vec4 rimColor {1.0f, 1.0f, 1.0f, 1.0f};
             float rust   = 0.0f;
             bool sirenOn = false;
             float steer  = 0.0f;
-            glm::vec4 tireColor {0.0f, 0.0f, 0.0f, 1.0f};
+            glm::vec4 tireColor {1.0f, 1.0f, 1.0f, 1.0f};
             glm::vec3 velocity {};
             glm::vec4 windowTint {0.0f, 0.0f, 0.0f, 20.0f / 255.0f};
         };
