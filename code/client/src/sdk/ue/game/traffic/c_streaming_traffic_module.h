00013ff8aaa0334713d713af3503e2ea344fa12e
Deewarz <deewarz@gmail.com>
2024-02-02 01:22:16 +0100

    fix pads

--- code/client/src/sdk/ue/game/traffic/c_streaming_traffic_module.h
+++ code/client/src/sdk/ue/game/traffic/c_streaming_traffic_module.h
@@ -7,6 +7,13 @@
 namespace SDK {
     namespace ue::game::traffic {
         class C_StreamingTrafficModule: public C_TickedModule {
+          private:
+            char pad0[0x8C];         // 0008 - 0094
+            int m_SeasonID;          // 0094 - 0098
+            int m_PrevSeasonID;      // 0098 - 009C
+            char pad1[0x64];         // 009C - 0100
+            int m_iMaxHumanElements; // 0100 - 0104
+
           public:
             /**
              * @param destroyActorItems see mafia::streaming::C_ActorsSlotWrapper::Close
@@ -47,13 +54,6 @@ namespace SDK {
             static C_StreamingTrafficModule *GetInstance() {
                 return hook::call<C_StreamingTrafficModule *>(gPatterns.C_StreamingTrafficModule__GetInstance);
             }
-
-          private:
-            char pad_0008[0x8C];     // 0008 - 0094
-            int m_SeasonID;          // 0094 - 0098
-            int m_PrevSeasonID;      // 0098 - 009C
-            char pad_009C[0x19C];    // 009C - 100
-            int m_iMaxHumanElements; // 100 - 104
         };
     } // namespace ue::game::traffic
 } // namespace SDK
