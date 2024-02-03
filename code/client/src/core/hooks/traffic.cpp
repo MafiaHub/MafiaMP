e260bf42160c08bd0edb629a4d910aba2349ba9c
Deewarz <72361937+Deewarz@users.noreply.github.com>
2024-02-03 22:35:24 +0100

    Reverse traffic / seasons stuffs (#78)
    * Reverse traffic / seasons stuffs
    
    * Fix E_TrafficVehicleFlags 0x800000
    
    * fix pad name
    
    * fix pads
    
    * Return void for C_StreamingTrafficModule::SetMaxHumanElements
    
    * Set fields as public
    
    * Fixed used notation
    
    ---------
    
    Co-authored-by: Segfault <5221072+Segfaultd@users.noreply.github.com>

--- code/client/src/core/hooks/traffic.cpp
+++ code/client/src/core/hooks/traffic.cpp
@@ -1,8 +1,13 @@
-#include <utils/hooking/hooking.h>
+#include "sdk/patterns.h"
+
 #include <utils/hooking/hook_function.h>
 
 static InitFunction init([]() {
-    //Disable traffic
-    const auto OpenSeasonAddr = hook::pattern("40 55 41 56 48 83 EC 48 83 B9 ? ? ? ? ?").get_first();
-    hook::return_function(OpenSeasonAddr);
+    /**
+     * Disable traffic
+     *
+     * Traffic is automaticaly loaded by the game via C_StreamingTrafficModule::OpenSeason
+     * after C_StreamMap::OpenPart("freeride") is called.
+     */
+    hook::return_function(SDK::gPatterns.C_StreamingTrafficModule__OpenSeason);
 });
