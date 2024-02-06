8821b184f7fc3682c767c6b99b15985016feba04
Segfault <5221072+Segfaultd@users.noreply.github.com>
2024-02-07 00:39:23 +0100

    Update human.cpp

--- code/client/src/game/helpers/human.cpp
+++ code/client/src/game/helpers/human.cpp
@@ -6,6 +6,8 @@
 #include "sdk/entities/c_car.h"
 #include "sdk/entities/c_human_2.h"
 
+#include "sdk/c_game.h"
+
 namespace MafiaMP::Game::Helpers {
     uint8_t Human::GetHealthPercent(SDK::C_Human2 *human) {
         float fHealth    = human->GetHumanScript()->GetHealth();
@@ -23,6 +25,18 @@ namespace MafiaMP::Game::Helpers {
             return false;
         }
 
+        const auto pLocalPlayer = SDK::GetGame()->GetActivePlayer();
+        if (!pLocalPlayer) {
+            return false;
+        }
+
+        // Specific code for local player that has no character controller bound
+        if (!charController || charController->GetCharacter()->GetType() == SDK::E_EntityType::E_ENTITY_PLAYER) {
+            SDK::ue::C_CntPtr<uintptr_t> syncObject;
+            reinterpret_cast<SDK::C_Human2 *>(pLocalPlayer)->GetHumanScript()->GetOnVehicle(syncObject, car, seat, true, true, SDK::E_HumanMoveMode::E_HMM_WALK, force);
+            return true;
+        }
+
         SDK::C_Actor *act = *(SDK::C_Actor **)((uintptr_t)car + 0xA8);
         return charController->TriggerActorAction(act, SDK::E_AA_ENTER_CAR, seat, force, false);
     }
@@ -30,6 +44,18 @@ namespace MafiaMP::Game::Helpers {
     bool Human::RemoveFromCar(MafiaMP::Game::Overrides::CharacterController *charController, SDK::C_Car *car, bool force) {
         if (!car) {
             return false;
+        }
+
+        const auto pLocalPlayer = SDK::GetGame()->GetActivePlayer();
+        if (!pLocalPlayer) {
+            return false;
+        }
+
+        // Specific code for local player that has no character controller bound
+        if (!charController || charController->GetCharacter()->GetType() == SDK::E_EntityType::E_ENTITY_PLAYER) {
+            SDK::ue::C_CntPtr<uintptr_t> syncObject;
+            reinterpret_cast<SDK::C_Human2 *>(pLocalPlayer)->GetHumanScript()->ScrDoAction(syncObject, car, SDK::E_AA_LEAVE_CAR, 0, false, true, SDK::E_HumanMoveMode::E_HMM_WALK, true);
+            return true;
         }
 
         SDK::C_Actor *act = *(SDK::C_Actor **)((uintptr_t)car + 0xA8);
