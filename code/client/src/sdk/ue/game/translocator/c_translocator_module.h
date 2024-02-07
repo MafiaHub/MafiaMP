c67d386448b576c54d49c240f0809ea246597d3f
Segfault <5221072+Segfaultd@users.noreply.github.com>
2024-02-07 22:06:56 +0100

    Translocator structure

--- code/client/src/sdk/ue/game/translocator/c_translocator_module.h
+++ code/client/src/sdk/ue/game/translocator/c_translocator_module.h
@@ -6,7 +6,7 @@
 namespace SDK {
     namespace ue::game::translocator {
         class C_TranslocatorModule {
-          private:
+          public:
             char pad0[0x8];                                                                     // 0000 - 0008
             ue::game::translocator::C_TranslocableObjectSpawner* m_pTranslocableObjectSpawner;  // 0008 - 0010
             ue::game::translocator::C_Translocator* m_pTranslocator;                            // 0010 - 0018
