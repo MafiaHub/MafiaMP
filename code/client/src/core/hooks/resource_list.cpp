f3f9f0d7bcc71a90e7a056bc299a42ff8ae00ad2
Segfault <5221072+Segfaultd@users.noreply.github.com>
2025-01-02 15:26:21 +0100

    More debug playground

--- code/client/src/core/hooks/resource_list.cpp
+++ code/client/src/core/hooks/resource_list.cpp
@@ -5,6 +5,7 @@
 #include <logging/logger.h>
 
 #include "sdk/ue/sys/utils/c_hash_name.h"
+#include "sdk/ue/c_string.h"
 #include "sdk/ue/sys/core/c_core.h"
 #include "sdk/ue/sys/core/c_resource_list.h"
 
@@ -28,10 +29,21 @@ int64_t C_ResourceList__LoadResource(void *pThis, SDK::ue::sys::utils::C_HashNam
     return C_ResourceList__LoadResource_original(pThis, name, stream, a3, params, a5);
 }
 
+typedef bool(__fastcall *C_ResourceList__IsResourceLoaded_t)(void *, SDK::ue::C_String const &);
+C_ResourceList__IsResourceLoaded_t C_ResourceList__IsResourceLoaded_original = nullptr;
+
+bool C_ResourceList__IsResourceLoaded(void *pThis, SDK::ue::C_String const &name) {
+    Framework::Logging::GetLogger("Hooks")->debug("Is Resource Loaded");
+    return C_ResourceList__IsResourceLoaded_original(pThis, name);
+}
+
 static InitFunction init([]() {
     // const auto C_ResourceList__LoadConfigXml_Addr = reinterpret_cast<uint64_t>(hook::pattern("40 53 48 81 EC ? ? ? ? 48 8B D9 C6 44 24 ? ? 48 8D 4C 24").get_first());
     // MH_CreateHook((LPVOID)C_ResourceList__LoadConfigXml_Addr, (PBYTE)C_ResourceList__LoadConfigXml, reinterpret_cast<void **>(&C_ResourceList__LoadConfigXml_original));
 
     // const auto C_ResourceList__LoadResource_Addr = reinterpret_cast<uint64_t>(hook::pattern("4C 89 44 24 ? 48 89 54 24 ? 55 57 41 54 41 56").get_first());
     // MH_CreateHook((LPVOID)C_ResourceList__LoadResource_Addr, (PBYTE)C_ResourceList__LoadResource, reinterpret_cast<void **>(&C_ResourceList__LoadResource_original));
+
+    // const auto C_ResourceList__IsResourceLoaded_Addr = reinterpret_cast<uint64_t>(hook::pattern("48 89 5C 24 ? 57 48 83 EC ? 48 8B 12 48 8B F9 48 85 D2 48 8D 4C 24").get_first());
+    // MH_CreateHook((LPVOID)C_ResourceList__IsResourceLoaded_Addr, (PBYTE)C_ResourceList__IsResourceLoaded, reinterpret_cast<void **>(&C_ResourceList__IsResourceLoaded_original));
 });
