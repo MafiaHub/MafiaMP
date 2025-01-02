f5a2665784af850fba94d5100effaa0a73454e16
Segfault <5221072+Segfaultd@users.noreply.github.com>
2025-01-02 12:52:11 +0100

    More inventory related class defs

--- code/client/src/sdk/c_inventory_item.h
+++ code/client/src/sdk/c_inventory_item.h
@@ -1,7 +1,14 @@
 #pragma once
 
 namespace SDK {
-    class C_ItemData {};
+    class I_ItemData {};
+    // TODO: check if it's C_ItemBase or C_ItemDataInventoryItem
+    class C_ItemData: public I_ItemData {
+      public:
+        uint64_t m_someSize; // 0008 - 0010
+        uint32_t m_itemId;   // 000C - 0010
+        uint64_t m_unknown;  // 0010 - 0018
+    };
 
     class I_InventoryItem {
       public:
@@ -10,7 +17,11 @@ namespace SDK {
 
     class C_InventoryItem : public I_InventoryItem {
       public:
-        C_ItemData *m_itemData;           // 0008 - 000F
-        uint64_t *m_itemStats;            // 0018 - 001F
+        C_ItemData *m_itemData;             // 0008 - 0010
+        bool m_dropable;                    // 0010 - 0011
+        bool m_changed;                     // 0011 - 0012
+        uint16_t _padding;                  // 0012 - 0014
     };
+
+    // static_assert(sizeof(C_InventoryItem) == 0x14, "Wrong size: C_InventoryItem");
 }
