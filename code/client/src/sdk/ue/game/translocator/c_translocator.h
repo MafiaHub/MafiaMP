e5d013c6e1a7fee70ad99d334ab71f9bd4f40115
Segfault <5221072+Segfaultd@users.noreply.github.com>
2025-01-02 16:52:41 +0100

    Fixed translocator data structure + reference

--- code/client/src/sdk/ue/game/translocator/c_translocator.h
+++ code/client/src/sdk/ue/game/translocator/c_translocator.h
@@ -1,18 +1,19 @@
 #pragma once
 
-#include "../../c_time_budget.h"
-#include "../../sys/math/c_vector.h"
+#include <cstdint>
+
+#include "sdk/ue/c_time_budget.h"
+#include "sdk/ue/sys/math/c_vector.h"
 
 namespace SDK {
     namespace ue::game::translocator {
         class C_Translocator {
           public:
             struct S_ObjectToSpawn {
-                char pad0[0x8];
-                void *m_pUnk;                           // 0008 - 0010
-                ue::sys::math::C_Vector m_vPosition;    // 0010 - 001C
-                char pad1[0x10];                        // 001C - 002C
-                int m_nObjectID;                        // 002C - 0030
+                void *pObject;                    // 0000 - 0008
+                uint32_t index;                   // 0x08 - 000C
+                uint32_t unk1;                    // 0x0C - 0010
+                SDK::ue::sys::math::C_Vector pos; // 0x10 - 001C
             };
             void SpawnObject(S_ObjectToSpawn const& object);
             void SpawnObjects(ue::C_TimeBudget const& timeBudget);
@@ -20,5 +21,7 @@ namespace SDK {
             private:
                 char pad0[0x48];            /// 0000 - 0048
         };
+
+        static_assert(sizeof(C_Translocator::S_ObjectToSpawn) == 0x20, "C_Translocator::S_ObjectToSpawn is not the correct size");
     }
 }
--- code/client/src/sdk/ue/game/translocator/c_translocator_module.h
+++ code/client/src/sdk/ue/game/translocator/c_translocator_module.h
@@ -3,11 +3,12 @@
 #include "c_translocable_object_spawner.h"
 #include "c_translocator.h"
 
+#include "sdk/c_ticked_module.h"
+
 namespace SDK {
     namespace ue::game::translocator {
-        class C_TranslocatorModule {
+        class C_TranslocatorModule : public C_TickedModule {
           public:
-            char pad0[0x8];                                                                     // 0000 - 0008
             ue::game::translocator::C_TranslocableObjectSpawner* m_pTranslocableObjectSpawner;  // 0008 - 0010
             ue::game::translocator::C_Translocator* m_pTranslocator;                            // 0010 - 0018
         };
